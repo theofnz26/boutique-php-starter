c56dc9c18e1ac295ba3086a36df5e108286997e7
theofnz26 <theo.fernandez@le-campus-numerique.fr>
2026-01-06 10:12:48 +0100

    Exo 4 : Les types et leurs pièges

---
+++ exercices/jour-01/types.php
@@ -0,0 +1,19 @@
+<?php
+
+//TEST 1
+$a = "5";
+$b = 3;
+$c = $a + $b;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+
+echo "\n~~~~~~~~~~~~~\n";
+
+
+// --- TEST 2 ---
+$price = "29.99€";
+$result = $price + 10;
+var_dump($result);
